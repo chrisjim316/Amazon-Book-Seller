b4cf2a04efbd43bfa9c9e09fa652c466760d431b
Chris Jim <chrisjim316@gmail.com>
2017-08-11 16:03:34 +0800

    remove intersection listener and add keypress event

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
+/* making sure collapse works */
 $(document).on('click.bs.collapse.data-api touchstart.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) { 
 });
-
-var io = new IntersectionObserver(
-    entries => {
-        console.log(entries);
-    },
-    {
-       
-    }
-);
-io.observe(element);
 
 function searchForBooks() {
   // Declare variables
@@ -18,21 +9,26 @@ function searchForBooks() {
 
   filter = search.val().toUpperCase();
   bookList = document.getElementsByClassName("well");
-  console.log(bookList);
+  //console.log(bookList);
 
   //clear previous results to search for a second time.
   if (search.val() == ""){
-    for(var i = 0; i < bookList.length; i++) {
+    for(var i = 0; i < bookList.length; i++) { 
       bookList[i].style.display = "";
     }
   }
   
+  search.keypress(function (e) {
+    /* when enter key is pressed */
+    if (e.which == 13) {
+      return false; //prevent refreshing the page every time enter key is pressed. 
+    }
+  });
+  
   // Loop through all list items, and hide those who don't match the search query
   for (var i = 0; i < bookList.length; i++) {
     title = bookList[i].innerText;
-    if (title.toUpperCase().indexOf(filter) >= 0) {
-      console.log(bookList[i]);
-    } else {
+    if (title.toUpperCase().indexOf(filter) == -1) {
       bookList[i].style.display = "none";
     }
   }
